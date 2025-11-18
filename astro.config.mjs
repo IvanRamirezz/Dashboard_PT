dc42151de8e0e482c8b7267f9009bbd3ff442f90
IvanRamirezz <92224804+IvanRamirezz@users.noreply.github.com>
2025-11-17 19:00:23 -0600

    Cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
 
@@ -8,9 +7,6 @@ export default defineConfig({
   site: 'https://ivanramirezz.github.io',
   base: '/Dashboard_PT/',
   output: 'server',
-  adapter: node({
-    mode: 'standalone', // o 'middleware'
-  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
